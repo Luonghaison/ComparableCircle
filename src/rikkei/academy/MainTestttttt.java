9fee620ea2c7b41a05dd9d9ae3df451804a05a60
unknown <keibin.wru@gmail.com>
2023-03-16 16:07:36 +0700

    2

---
+++ src/rikkei/academy/MainTestttttt.java
@@ -0,0 +1,9 @@
+package rikkei.academy;
+
+public class MainTestttttt {
+    public static void main(String[] args) {
+        ComparableCircle comparableCircle1 = new ComparableCircle();
+        ComparableCircle comparableCircle2 = new ComparableCircle();
+        comparableCircle1.compareTo(comparableCircle2);
+    }
+}
